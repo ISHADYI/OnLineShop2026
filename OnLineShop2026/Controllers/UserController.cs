d6969637bbaeeff1f6d95ed0a9fb84c53bbf55f9
ISHADYI <shadyworkq@gmail.com>
2026-03-29 23:38:32 +0300

    Создала конроллер, где создается один пользователь и передается во View

---
+++ OnLineShop2026/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnLineShop2026.Models;
+
+namespace OnLineShop2026.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            User user = new User(
+                "Анастасия",
+                "Иванова",
+                "shadyworkq@gmail.com",
+                "/images/avatar.jpg"
+            );
+
+            return View(user);
+        }
+    }
+}
