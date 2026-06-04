709c63786b1cd5a693fa1b6eecb77e3a3f67f4d6
ISHADYI <shadyworkq@gmail.com>
2026-06-04 22:44:28 +0300

    Создание репозитория пользователей

--- OnLineShop2026/Controllers/UserController.cs
+++ OnLineShop2026/Controllers/UserController.cs
@@ -1,10 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using OnLineShop2026.Models;
+using OnLineShop2026.Data;
 
 namespace OnLineShop2026.Controllers
 {
     public class UserController : Controller
     {
+        private readonly IUserRepository _userRepository;
+
+        public UserController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
         public IActionResult Index()
         {
             User user = new User(
@@ -15,6 +23,13 @@ namespace OnLineShop2026.Controllers
             );
 
             return View(user);
+        }
+
+        public IActionResult List()
+        {
+            var users = _userRepository.GetAll();
+
+            return View(users);
         }
     }
 }
---
+++ OnLineShop2026/Data/UserRepositoryInMemory.cs
@@ -0,0 +1,24 @@
+﻿using OnLineShop2026.Models;
+
+namespace OnLineShop2026.Data
+{
+    public class UserRepositoryInMemory : IUserRepository
+    {
+        private static List<User> users = new List<User>()
+        {
+            new User("Анастасия", "Иванова", "shadyworkq@gmail.com", "/images/avatar.jpg"),
+            new User("Яна", "Кайтукова", "yana@gmail.com", "/images/avatar.jpg"),
+            new User("Арина", "Цагараева", "arinayo@gmail.com", "/images/avatar.jpg"),
+        };
+
+        public List<User> GetAll()
+        {
+            return users;
+        }
+
+        public User? TryGetById(Guid id)
+        {
+            return users.FirstOrDefault(user => user.Id == id);
+        }
+    }
+}
